bf5b073267ce22885f98590c9927544d06fa127b
unknown <marcosgonzalezpenin@gmail.com>
2022-06-27 09:35:36 +0200

    Primera historia completada

--- gestionHoteles-model/src/main/java/com/campusdual/fisionnucelar/gestionHoteles/model/core/service/HotelService.java
+++ gestionHoteles-model/src/main/java/com/campusdual/fisionnucelar/gestionHoteles/model/core/service/HotelService.java
@@ -15,35 +15,54 @@ import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 
 @Service("HotelService")
 @Lazy
-public class HotelService implements IHotelService{
+public class HotelService implements IHotelService {
 
- @Autowired private HotelDao hotelDao;
- @Autowired private DefaultOntimizeDaoHelper daoHelper;
- 
+ @Autowired
+ private HotelDao hotelDao;
+ @Autowired
+ private DefaultOntimizeDaoHelper daoHelper;
+
  @Override
  public EntityResult hotelQuery(Map<String, Object> keyMap, List<String> attrList)
    throws OntimizeJEERuntimeException {
-  return this.daoHelper.query(this.hotelDao, keyMap, attrList);
+  EntityResult searchResult = this.daoHelper.query(this.hotelDao, keyMap, attrList);
+  if (searchResult.OPERATION_WRONG == 0) {
+   searchResult.setMessage("No se ha encontrado ningún resultado");
+  }
+  return searchResult;
  }
 
  @Override
  public EntityResult hotelInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
- // return this.daoHelper.insert(this.hotelDao, attrMap);
-//	 attrMap.clear();
-//	 //attrMap.put("id_hotel", 5);
-//	 attrMap.put("htl_nombre","Hotel");
-	 return this.daoHelper.insert(this.hotelDao, attrMap);
+  EntityResult insertResult = this.daoHelper.insert(this.hotelDao, attrMap);
+  if (insertResult.OPERATION_SUCCESSFUL == 0) {
+   insertResult.setMessage("Inserción realizada correctamente");
+  } else {
+   insertResult.setMessage("Error. No se ha podido realizar la inserción");
+  }
+  return insertResult;
  }
 
  @Override
  public EntityResult hotelUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
    throws OntimizeJEERuntimeException {
-  return this.daoHelper.update(this.hotelDao, attrMap, keyMap);
+  EntityResult updateResult = this.daoHelper.update(this.hotelDao, attrMap, keyMap);
+  if (updateResult.OPERATION_SUCCESSFUL == 0) {
+   updateResult.setMessage("Actualización realizada correctamente");
+  } else {
+   updateResult.setMessage("Error. No se ha podido realizar la actualización");
+  }
+  return updateResult;
  }
 
  @Override
  public EntityResult hotelDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
-  return this.daoHelper.delete(this.hotelDao, keyMap);
+  EntityResult deleteResult = this.daoHelper.delete(this.hotelDao, keyMap);
+  if (deleteResult.OPERATION_SUCCESSFUL == 0) {
+   deleteResult.setMessage("Borrado realizado correctamente");
+  } else {
+   deleteResult.setMessage("Error. No se ha podido realizar el borrado");
+  }
+  return deleteResult;
  }
-
 }
