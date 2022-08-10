1c6b44e5d03ee8d6f7c530bd70c558d031b3d354
Samuel Purriños <samuel.purrinos@outlook.com>
2022-08-10 13:34:02 +0200

    Añade endpoint para crear recibos desde la tabla de booking_hist

--- gestionHoteles-model/src/main/java/com/campusdual/fisionnucelar/gestionHoteles/model/core/exception/InvalidPhoneException.java
+++ gestionHoteles-model/src/main/java/com/campusdual/fisionnucelar/gestionHoteles/model/core/exception/InvalidPhoneException.java
@@ -3,4 +3,5 @@ package com.campusdual.fisionnucelar.gestionHoteles.model.core.exception;
 public class InvalidPhoneException extends RuntimeException {
 	public InvalidPhoneException(String errorMessage) {
 		super(errorMessage);
+	}
 }
